05af9dad4e298086f8804bb0a75890a6a52d0934
diegonacif <diegoreisnacif@gmail.com>
2021-05-31 22:10:58 -0300

    Adicionando rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+import Router from './Router'
 import './App.css';
+import {BrowserRouter, Route, Switch, Link} from 'react-router-dom'
+import Books from './pages/books/Books'
+import Tools from './pages/tools/Tools'
+import Main from './pages/Main/Main'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <BrowserRouter>
+        <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Raíz</Link>
+            </li>
+            <li>
+              <Link to="/Books">Livros</Link>
+            </li>
+            <li>
+              <Link to="/Tools">Ferramentis</Link>
+            </li>
+          </ul>
+        </nav>
+        </div>
+        <Switch>
+          <Route exact path="/"component={Main} />
+          <Route exact path="/Books"component={Books} />
+          <Route exact path="/Tools"component={Tools} />
+        <Books />
+        </Switch>
+        </BrowserRouter>
       </header>
     </div>
   );
