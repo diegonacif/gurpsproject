35d0883943ad407a18b1b935284f2bb78629e00b
diegonacif <diegoreisnacif@gmail.com>
2021-06-02 01:12:38 -0300

    Desenvolvimento da pagina Livros

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,31 @@
-import React from 'react';
-import Router from './Router'
+import Router from './Router';
 import './App.css';
-import {BrowserRouter, Route, Switch, Link} from 'react-router-dom'
-import Books from './pages/books/Books'
-import Tools from './pages/tools/Tools'
-import Main from './pages/Main/Main'
-
+import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
+import Books from './pages/books/Books';
+import Tools from './pages/tools/Tools';
+import Main from './pages/Main/Main';
+import {Dropdown, DropdownButton, ButtonGroup} from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <BrowserRouter>
-        <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Raíz</Link>
-            </li>
-            <li>
-              <Link to="/Books">Livros</Link>
-            </li>
-            <li>
-              <Link to="/Tools">Ferramentis</Link>
-            </li>
-          </ul>
-        </nav>
-        </div>
+        {/* Dropdown Menu */}
+        <Dropdown>
+
+        <DropdownButton title="Navegador">
+          <Dropdown.Item as="button"><Link to="/">Raíz</Link></Dropdown.Item>
+          <Dropdown.Item as="button"><Link to="/Books">Livros</Link></Dropdown.Item>
+          <Dropdown.Item as="button"><Link to="/Tools">Ferramentis</Link></Dropdown.Item>
+        </DropdownButton>       
+          
+        </Dropdown>
+
         <Switch>
           <Route exact path="/"component={Main} />
           <Route exact path="/Books"component={Books} />
-          <Route exact path="/Tools"component={Tools} />
-        <Books />
+          <Route exact path="/Tools"component={Tools} />          
         </Switch>
         </BrowserRouter>
       </header>
--- src/pages/books/Books.tsx
+++ src/pages/books/Books.tsx
@@ -1,10 +1,126 @@
 import * as React from 'react';
 import { Link, RouteComponentProps } from 'react-router-dom';
+import './Books.css';
+import * as ImgSrc from '../../images'
 
 const Books = () => {
   return (
     <div>
-      <p>Eu estou na página dos livros!</p>
+      <h1>Livros</h1>
+      <div className="main">
+        <div className="row">
+          <div className="column">
+            <div className="content">
+              <a href="https://mega.nz/file/755m1SSa#VsQjzoejbtXNX9ay-P_kOhPYAxbFDp45UfefVv-u5Ls">
+                <img src={ImgSrc.gurps_personagens} alt="Mountains" className="bkimg" />
+              </a>
+              <div className="booknametwo">
+                  Módulo Básico Personagens
+              </div>
+            </div>
+          </div>
+          <div className="column">
+            <div className="content">
+              <a href="https://mega.nz/file/755m1SSa#VsQjzoejbtXNX9ay-P_kOhPYAxbFDp45UfefVv-u5Ls">
+                <img src={ImgSrc.gurps_lowtech} alt="Mountains" className="bkimg" />
+              </a>
+              <div className="bookname">
+                  Low Tech
+              </div>
+            </div>
+          </div>
+          <div className="column">
+            <div className="content">
+              <a href="https://mega.nz/file/755m1SSa#VsQjzoejbtXNX9ay-P_kOhPYAxbFDp45UfefVv-u5Ls">
+                <img src={ImgSrc.gurps_martialarts} alt="Mountains" className="bkimg" />
+              </a>
+              <div className="bookname">
+                  Martial Arts
+              </div>
+            </div>
+          </div>
+          <div className="column">
+            <div className="content">
+              <a href="https://mega.nz/file/755m1SSa#VsQjzoejbtXNX9ay-P_kOhPYAxbFDp45UfefVv-u5Ls">
+                <img src={ImgSrc.gurps_magic} alt="Mountains" className="bkimg" />
+              </a>
+              <div className="bookname">
+                  Magic
+              </div>
+            </div>
+          </div>
+          <div className="column">
+            <div className="content">
+              <a href="https://mega.nz/file/755m1SSa#VsQjzoejbtXNX9ay-P_kOhPYAxbFDp45UfefVv-u5Ls">
+                <img src={ImgSrc.gurps_thaumatology} alt="Mountains" className="bkimg" />
+              </a>
+              <div className="bookname">
+                  Thaumatology
+              </div>
+            </div>
+          </div>
+          <div className="column">
+            <div className="content">
+              <a href="https://mega.nz/file/755m1SSa#VsQjzoejbtXNX9ay-P_kOhPYAxbFDp45UfefVv-u5Ls">
+                <img src={ImgSrc.gurps_fantasy} alt="Mountains" className="bkimg" />
+              </a>
+              <div className="bookname">
+                  Fantasy
+              </div>
+            </div>
+          </div>
+          <div className="column">
+            <div className="content">
+              <a href="https://mega.nz/file/755m1SSa#VsQjzoejbtXNX9ay-P_kOhPYAxbFDp45UfefVv-u5Ls">
+                <img src={ImgSrc.gurps_banestorm} alt="Mountains" className="bkimg" />
+              </a>
+              <div className="bookname">
+                  Banestorm
+              </div>
+            </div>
+          </div>
+          <div className="column">
+            <div className="content">
+              <a href="https://mega.nz/file/755m1SSa#VsQjzoejbtXNX9ay-P_kOhPYAxbFDp45UfefVv-u5Ls">
+                <img src={ImgSrc.gurps_escudodomestre} alt="Mountains" className="bkimg" />
+              </a>
+              <div className="booknametwo">
+                  Escudo do Mestre
+              </div>
+            </div>
+          </div>
+          <div className="column">
+            <div className="content">
+              <a href="https://mega.nz/file/755m1SSa#VsQjzoejbtXNX9ay-P_kOhPYAxbFDp45UfefVv-u5Ls">
+                <img src={ImgSrc.gurps_manobras} alt="Mountains" className="bkimg" />
+              </a>
+              <div className="bookname">
+                  Manobras
+              </div>
+            </div>
+          </div>
+          <div className="column">
+            <div className="content">
+              <a href="https://mega.nz/file/755m1SSa#VsQjzoejbtXNX9ay-P_kOhPYAxbFDp45UfefVv-u5Ls">
+                <img src={ImgSrc.gurps_imbuements} alt="Mountains" className="bkimg" />
+              </a>
+              <div className="bookname">
+                  Imbuements
+              </div>
+            </div>
+          </div>
+          <div className="column">
+            <div className="content">
+              <a href="https://mega.nz/file/755m1SSa#VsQjzoejbtXNX9ay-P_kOhPYAxbFDp45UfefVv-u5Ls">
+                <img src={ImgSrc.gurps_masscombat} alt="Mountains" className="bkimg" />
+              </a>
+              <div className="bookname">
+                  Mass Combat
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>      
     </div>
   );
 };
