89f52eb6a7d8337b314c01f192816cc796583c2c
diegonacif <diegoreisnacif@gmail.com>
2021-06-02 01:07:02 -0300

    Desenvolvimento da pagina Livros

---
+++ src/images/index.jsx
@@ -0,0 +1,31 @@
+import bluebg from './bluebg.jpg'
+import gurps_banestorm from './gurps_banestorm.png'
+import gurps_escudodomestre from './gurps_escudodomestre.png'
+import gurps_fantasy from './gurps_fantasy.png'
+import gurps_imbuements from './gurps_imbuements.png'
+import gurps_lowtech from './gurps_lowtech.png'
+import gurps_magic from './gurps_magic.png'
+import gurps_manobras from './gurps_manobras.png'
+import gurps_martialarts from './gurps_martialarts.png'
+import gurps_masscombat from './gurps_masscombat.png'
+import gurps_personagens from './gurps_personagens.png'
+import gurps_thaumatology from './gurps_thaumatology.png'
+import libraryblur from './libraryblur.jpg'
+import under_construction from './under_construction.png'
+
+export {
+  bluebg,
+  gurps_banestorm,
+  gurps_escudodomestre,
+  gurps_fantasy,
+  gurps_imbuements,
+  gurps_lowtech,
+  gurps_magic,
+  gurps_manobras,
+  gurps_martialarts,
+  gurps_masscombat,
+  gurps_personagens,
+  gurps_thaumatology,
+  libraryblur,
+  under_construction
+};
